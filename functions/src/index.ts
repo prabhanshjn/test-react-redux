3b2bf081a941851354945dcf77113e9a10bff776
Prabhansh Jain <95516177+prabhanshjn@users.noreply.github.com>
2022-12-22 11:54:41 +0530

    github deploy

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,3 +7,5 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", { structuredData: true })
   response.send("Hello from Firebase!")
 })
+
+console.log("function started")
